c117dd3b6415e824e552eb56aa3826cd897c1988
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-06-16 10:38:03 +0530

    Add express session middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+const MongoStore = require('connect-mongo').default
 const dbConnection = require('./config/db.connection')
 
 // Database Connection
@@ -12,5 +13,17 @@ dbConnection()
 app.use(express.json())
 
 app.use(express.urlencoded({ extended : true }))
+
+app.use(session({
+    secret : process.env.SESSION_SECRET_KEY,
+    resave : false,
+    saveUninitialized : false,
+    cookie : { secure : false, maxAge : 30 * 24 * 60 * 60 * 1000 },
+    store : MongoStore.create({
+        mongoUrl : process.env.MONGODB_CONNECTION_STRING,
+        collectionName : 'sessions',
+        ttl : 30 * 24 * 60 * 60
+    }) 
+}))
 
 module.exports = app
