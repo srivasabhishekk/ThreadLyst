751e6f1623b02f1bc2dfc41007f346d85159fa0e
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-07-06 13:05:58 +0530

    Removed Express Session Middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const session = require('express-session')
-const MongoStore = require('connect-mongo').default
 const dbConnection = require('./config/db.connection')
+const authRouter = require('./routes/auth.route')
 
 // Database Connection
 
@@ -14,16 +13,8 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended : true }))
 
-app.use(session({
-    secret : process.env.SESSION_SECRET_KEY,
-    resave : false,
-    saveUninitialized : false,
-    cookie : { secure : false, maxAge : 30 * 24 * 60 * 60 * 1000 },
-    store : MongoStore.create({
-        mongoUrl : process.env.MONGODB_CONNECTION_STRING,
-        collectionName : 'sessions',
-        ttl : 30 * 24 * 60 * 60
-    }) 
-}))
+// Routes
+
+app.use('/api/auth', authRouter)
 
 module.exports = app
