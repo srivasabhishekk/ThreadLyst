0cb148e0f292280125fc4600f9a04e74b4b3214b
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-26 11:13:40 +0530

    feat: add user model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    googleId : {
+        type : String, required : true
+    },
+    email : {
+        type : String, required : true, unique : true
+    },
+    accessToken : {
+        type : String,
+        required : true
+    },
+    refreshToken : {
+        type : String,
+        required : true
+    },
+    expiryTime : {
+        type : Number,
+        required : true
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
