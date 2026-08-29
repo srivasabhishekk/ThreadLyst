0fc2386995c0f8a7d7206bee22cc49af622f8061
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-30 00:28:48 +0530

    feat: make googleUserId unique in user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     googleId : {
-        type : String, required : true
+        type : String, required : true, unique : true
     },
     email : {
         type : String, required : true, unique : true
